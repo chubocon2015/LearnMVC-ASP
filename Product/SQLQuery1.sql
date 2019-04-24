44d41657f0c346fbc88dc507027df69b46164f17
chubocon2015 <47852202+chubocon2015@users.noreply.github.com>
2019-04-25 01:12:25 +0700

    ProDuct
    Bài mới

---
+++ Product/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿create database Hoang
+use Hoang
+go
+create table Product (	
+	id int identity(1,1) primary key,
+	Name nvarchar(max) not null,
+	Price int not null,
+	Expire date not null, 
+	)
---
+++ Product/WebApplication1.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new HoangEntities();
+            var controller = new ProductControllerTest();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model,
+                typeof(List<Product>));
+            Assert.AreEqual(db.Products.Count(),
+                (result.Model as List<Product>).Count);
+        }
+        [TestMethod]
+        public void TestEdit()
+        {
+            var controller = new ProductControllerTest();
+            var result0 = controller.Edit(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+
+            var db = new HoangEntities();
+            var item = db.Products.First();
+            var result1 = controller.Edit(item.id) as ViewResult;
+            Assert.IsNotNull(result1);
+            var model = result1.Model as Product;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(item.id, model.id);
+        }
+    }
+}
